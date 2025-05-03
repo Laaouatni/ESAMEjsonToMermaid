bc479df0aa365c61a32afd4f542a1e13070e23f9
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 14:10:33 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-  import mermaid from 'mermaid';
-  import { onMount } from 'svelte';
-  
-  onMount(async () => {
-    mermaid.initialize({ startOnLoad: true });
-    const {svg} = await mermaid.render("mainElementSvg", `graph TB\na-->b`);
-    const mainElement = document.getElementById("mainElement");
-    if(!mainElement) return;
-    mainElement.innerHTML = svg
-  });
-</script>
+  import type { TypeMaterie, TypeArgomenti, TypeNucleiTematici } from '$lib/types/typeConstants';
 
-<main id="mainElement">
-  a
-</main>
+  type TypeCollegamenti = {
+    [thisTypeNucleoTematico in TypeNucleiTematici] : {
+      [thisTypeMateria in TypeMaterie]: TypeArgomenti<TypeMaterie>[]
+    }
+  };
+
+  const COLLEGAMENTI = {
+
+  }
+
+
+  // import mermaid from 'mermaid';
+  // import { onMount } from 'svelte';
+  
+  // onMount(async () => {
+  //   mermaid.initialize({ startOnLoad: true });
+  //   const {svg} = await mermaid.render("mainElementSvg", `graph TB\na-->b`);
+  //   const mainElement = document.getElementById("mainElement");
+  //   if(!mainElement) return;
+  //   mainElement.innerHTML = svg
+  // });
+</script>
