fdee8afa197d9c52637f5c73504774d2b062695d
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 13:14:08 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   const LISTA_ARGOMENTI = {
-    ITALIANO: ["MARINETTI", "UNGARETTI", "PASCOLI", "GABRIELE D'ANNUNZIO"],
+    ITALIANO: ["MARINETTI", "UNGARETTI", "PASCOLI", "GABRIELE D'ANNUNZIO"] as const,
     STORIA: [
       "SECONDA RIVOLUZIONE INDUSTRIALE",
       "FASCISMO",
       "CROLLO DELLA BORSA DI WALL STREET",
-    ],
+    ] as const,
   };
+
+  type TypeItaliano = typeof LISTA_ARGOMENTI.ITALIANO[number];
+  type TypeStoria = typeof LISTA_ARGOMENTI.STORIA[number];
 </script>
