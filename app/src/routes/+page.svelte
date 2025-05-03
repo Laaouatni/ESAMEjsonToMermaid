e792794b65c3ea97351b5ca9c482852fa63aaef7
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 14:00:52 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,3 +1,16 @@
 <script lang="ts">
+  import mermaid from 'mermaid';
+  import { onMount } from 'svelte';
   
+  onMount(async () => {
+    mermaid.initialize({ startOnLoad: true });
+    const {svg} = await mermaid.render("mainElementSvg", `graph TB\na-->b`);
+    const mainElement = document.getElementById("mainElement");
+    if(!mainElement) return;
+    mainElement.innerHTML = svg
+  });
 </script>
+
+<main id="mainElement">
+  a
+</main>
