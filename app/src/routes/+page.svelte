b5705e3272cb8c02eac73ed98d0dabbbe5299180
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 14:44:37 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,7 +4,35 @@
   const COLLEGAMENTI: TypeCollegamenti = {
     "AMBIENTE E ENERGIA": {
       ITALIANO: {
-        MARINETTI: ["L'ambiente della guerra -> Marinetti partecipa alla guerra di libia come giornalista… La guerra viene vista come uno spettacolo (Zang Tumb Tumb)"],
+        MARINETTI: [
+          "L'ambiente della guerra -> Marinetti partecipa alla guerra di libia come giornalista… La guerra viene vista come uno spettacolo (Zang Tumb Tumb)",
+        ],
+        UNGARETTI: [
+          "SONO UNA CREATURA -> mette in relazione le rocce asciutte del Carso con il suo pianto.",
+          "in generale qualsiasi poesia fatta dell'Allegria (Veglia, Natale, Soldati, Sono una creatura) perchè contiene il LUOGO e la data all’inizio di ogni poesia breve -> anche se da preferirsi SONO UNA CREATURA.",
+        ],
+        PASCOLI: [
+          "Lavandare (si parla di Aratro e poi tante onomatopee che ricordano il suono dell'ambiente come sciabordare)",
+        ],
+        "GABRIELE D'ANNUNZIO": [
+          "NOTTURNO -> SCRIVO NELL'OSCURITà (collegamento con l'energia, l'autore non vede e scrive con l'aiuto della figlia)",
+          "ALCYONE - PIOGGIA NEL PINETO -> Metaformismo Panico",
+        ],
+      },
+      STORIA: {
+        "SECONDA RIVOLUZIONE INDUSTRIALE": [
+          "Energia elettrica <-> es. simbolo progresso industriale",
+          "Se parto da Matematica Curva di KEELING (CO2) posso dire che la curva inizia a diventare crescente quando avviene la PRIMA RIVOLUZIONE INDUSTRIALE (però visto che abbiamo fatto la 2 RIVOLUZIONE INDUSTRIALE, inizia con una comparazione veloce la 1 RIV poipa parla solo della 2 RIV INDUSTRIALE)",
+        ],
+        "CONQUISTA DI FIUME DI D'ANNUNZIO": [
+          "partendo da ITA NOTTURNO -> SCRIVO NELL’OSCURITà lo collego con la CONQUISTA DI FIUME dove perde un occhio/vista momentaneamente (partendo da fine 1 guerra mondiale con il fallimento del tavolo delle paci di Orlando poi parlo dell’esclamazione “Vittoria Mutilata” poi conquista Fiume poi continuo con FASCISMO)",
+        ],
+        FASCISMO: [
+          "Una volta crollata in america la borsa di Wall Street anche l'italia risente le conseguenze e di conseguenza lo stato italiano fascista diventa imprenditore proteggendo Fiat e EDISON (energia)",
+        ],
+        "USA - CROLLO DELLA BORSA DI WALL STREET": [
+          "Una volta crollata in america la borsa di Wall Street anche l’italia risente le conseguenze e di conseguenza lo stato italiano fascista diventa imprenditore proteggendo Fiat e EDISON (energia).",
+        ],
       },
     },
   };
