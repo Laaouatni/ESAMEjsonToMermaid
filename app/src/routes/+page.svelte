cdde0ddec363545917ea241cfdf66c8a82790b16
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 18:31:08 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -63,7 +63,8 @@
                     nucleoTematico: idToGetTematica[thisNucleoTematico],
                   };
                   result += myString(
-                    `${thisIds.argomento}---->${thisSpiegazione == "" ? "" : `|"${thisSpiegazione.replace(/"/g, "'")}"|`}${thisIds.nucleoTematico}`,
+                    // `${thisIds.argomento}---->${thisSpiegazione == "" ? "" : `|"${thisSpiegazione.replace(/"/g, "'")}"|`}${thisIds.nucleoTematico}`,
+                    `${thisIds.argomento}---->${thisIds.nucleoTematico}`,
                   );
                   // result += myString(
                   //   `${thisIds.nucleoTematico}-->${thisIds.argomento}`,
