a29057d26957164c165b3ae8a8e189fe14f03b2b
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 14:36:07 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -8,15 +8,4 @@
       },
     },
   };
- 
-  // import mermaid from 'mermaid';
-  // import { onMount } from 'svelte';
-
-  // onMount(async () => {
-  //   mermaid.initialize({ startOnLoad: true });
-  //   const {svg} = await mermaid.render("mainElementSvg", `graph TB\na-->b`);
-  //   const mainElement = document.getElementById("mainElement");
-  //   if(!mainElement) return;
-  //   mainElement.innerHTML = svg
-  // });
 </script>
