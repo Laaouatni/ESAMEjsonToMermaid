a4a3c4aa19019e53adbd4500c891d98a7bac2c33
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 14:17:49 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
-  import type { TypeMaterie, TypeArgomenti, TypeNucleiTematici } from '$lib/types/typeConstants';
+  import type {
+    TypeMaterie,
+    TypeArgomenti,
+    TypeNucleiTematici,
+  } from "$lib/types/typeConstants";
 
   type TypeCollegamenti = {
-    [thisTypeNucleoTematico in TypeNucleiTematici] : {
-      [thisTypeMateria in TypeMaterie]: TypeArgomenti<TypeMaterie>[]
-    }
+    [thisTypeNucleoTematico in TypeNucleiTematici]: {
+      [thisTypeMateria in TypeMaterie]: {
+        [thisTypeArgomento in TypeArgomenti<thisTypeMateria>]: string[];
+      }[];
+    };
   };
 
   const COLLEGAMENTI: TypeCollegamenti = {
-    
-  }
-
+    "AMBIENTE E ENERGIA": {
+      ITALIANO: [
+        "MARINETTI": [
+          "L'ambiente della guerra -> Marinetti partecipa alla guerra di libia come giornalista… La guerra viene vista come uno spettacolo (Zang Tumb Tumb)"
+        ],
+      ],
+    },
+  };
 
   // import mermaid from 'mermaid';
   // import { onMount } from 'svelte';
-  
+
   // onMount(async () => {
   //   mermaid.initialize({ startOnLoad: true });
   //   const {svg} = await mermaid.render("mainElementSvg", `graph TB\na-->b`);
