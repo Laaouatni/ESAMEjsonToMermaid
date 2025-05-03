18d5a4eadf796d06966b7e61d19e688d7339c8a1
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-03 16:44:04 +0200

    save

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+	import { LISTA_ARGOMENTI } from '$lib/constants/listaArgomenti';
+	import { LISTA_NUCLEI_TEMATICI } from '$lib/constants/listaNucleiTematici';
   import { LISTA_COLLEGAMENTI } from "$lib/constants/listaCollegamenti";
 
-  console.log(LISTA_COLLEGAMENTI);
+  function myString(string) {
+    return `${string}\n`;
+  }
+
+  function generateMermaidString() {
+    let result = myString(`flowchart LR\n`)
+
+    result += myString("subgraph NUCLEI_TEMATICI['NUCLEI TEMATICI']");
+    LISTA_NUCLEI_TEMATICI.forEach((thisNucleoTematico, thisIndex) => {
+      result += myString(`\tTEMATICA_${thisIndex}['${thisNucleoTematico}']`);
+    })
+    result += myString("end");
+
+    return result;
+  };
+
+  console.log(generateMermaidString());
 </script>
